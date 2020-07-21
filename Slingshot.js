3150aa3cfe6febbc805525b3eae45f08aa4cae33
sahajSinghal <sahajsinghal2007@gmail.com>
2020-07-21 14:35:10 +0530

    Files

--- Slingshot.js
+++ Slingshot.js
@@ -14,6 +14,11 @@ class SlingShot{
         World.add(world, this.sling);
     }
 
+    attach(body)
+    {
+        this.sling.bodyA = body;
+    }
+
     fly(){
         this.sling.bodyA = null;
     }
--- sketch.js
+++ sketch.js
@@ -71,7 +71,14 @@ function mouseDragged(){
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
 }
 
-
 function mouseReleased(){
     slingshot.fly();
+}
+
+function keyPressed()
+{
+    if(keyCode === 32)
+    {
+        slingshot.attach(bird.body);
+    }
 }
